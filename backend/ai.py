72e5f1c1cd16ffd66c8b0c746089e42a23ac91c5
azacha <a.zacharuk@outlook.de>
2020-01-08 13:34:29 +0100

    refactor(backend): softmax survivor selection,dynamic mutation rate

--- backend/ai.py
+++ backend/ai.py
@@ -5,6 +5,7 @@ Created on Fri Jan  3 18:46:02 2020
 @author: azach
 """
 import os
+import glob
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
 import sys
@@ -30,22 +31,25 @@ import random
 
 class AI():
     def __init__(self):
-        self.FilePath = "./models/"
+        self.FilePathModels = "./models/"
         self.FilePathLog = "./log/"
         self.width = 50 #width of input matrix
         self.height = 50 #height of input matrix
         self.levels = 4 # number of classes in matrix
         self.n_auxData = 4 #aux data
-        self.mutationRate = 0.05 #standard devivation for selection from normal distrubtion on Generation > 0" 
+        self.mutationRate = 0.5 #standard devivation for selection from normal distrubtion on Generation > 0" 
         self.variance = 1 #standard devivation for selection from normal distrubtion on Generation = 0" 
         self.network = []
-        self.survivorRate = 8
+        self.nonSurvivorRate = 0.0 #
+        self.selectionRate = 0.25 # keep top percentage
         self.totalEnergyIntake = dict({})
         self.EnergyIntake = {}
         self.generation = 0
-        self.selectionRate = 0.1 # keep top percentage
+        self.Counter = 0
+        self.AdaptionFrequency = 50 
+        self.AdaptionRate = 0.5 
         
-
+        
     '''Functions to extract data from messages,
     reshape inputs and create tensor'''
 
@@ -56,36 +60,19 @@ class AI():
         df['individuum'] = df['individuum'].astype(str)
         population = df['individuum'].unique()
         return df, population
-
-    def trackEnergyIntake(self,df):
-        currentEnergyIntake = dict(zip(df["individuum"],df["energyIntake"]))
-        
-        if not self.totalEnergyIntake:
-            self.EnergyIntake = dict(zip(df["individuum"],([0] * len(df))))
-        else:
-            for k, v in currentEnergyIntake.items():
-                self.EnergyIntake[k] = v - self.totalEnergyIntake.get(k, 0)
-            #map energyIntake to 0 or 1
-            for key, value in self.EnergyIntake.items():
-                if value > 0:
-                    self.EnergyIntake[key] = 1
-        self.totalEnergyIntake.update(currentEnergyIntake)
-
-        return self
-
+    
     #create input tensors
     '''extract values from dataframe and turns it arrays'''
-
+    def getAuxInput(self,IndividualEnergyIntake,energyLevel,velocityX,velocityY):
+        return np.asarray([IndividualEnergyIntake,energyLevel,velocityX,velocityY])
+   
     def createInputs(self, df,individuum):
         #subset df to single snake data and put values into input arrays
         df_subset = df[df["individuum"] == individuum]
-
-        IndividualEnergyIntake = self.EnergyIntake[str(individuum)]
-      
-        
-        auxInput =np.asarray([IndividualEnergyIntake,df_subset["energyLevel"].values[0],df_subset["velocityX"].values[0],df_subset["velocityY"].values[0]])
+        IndividualEnergyIntake =self.totalEnergyIntake[individuum]
+        auxInput =self.getAuxInput(IndividualEnergyIntake,df_subset["energyLevel"].values[0],df_subset["velocityX"].values[0],df_subset["velocityY"].values[0])
         inputArray =np.asarray(df_subset["matrix"].values[0])
-        inputArray = inputArray/self.levels
+        #inputArray = inputArray/self.levels
         return inputArray, auxInput
 
     ##reshaping
@@ -104,9 +91,11 @@ class AI():
         auxiliaryInput = Input(shape=(self.n_auxData,), name='aux_input')
 
         #CNN Network for processing matrix Data
-        x = Conv2D(32, (3, 3), padding='same')(mainInput)
+        x = Conv2D(64, (3, 3), padding='same',activation ='relu')(mainInput)
         x = MaxPooling2D((2, 2))(x)
-        CNNout = Flatten()(x)
+        x = Conv2D(32, (3, 3), padding='same',activation ='relu')(mainInput)
+        x= Flatten()(x)
+        CNNout= Dense(units = 4, activation = 'relu')(x)
         #combine CNN Output with metaInput
         x = concatenate([CNNout, auxiliaryInput])
         #stack a deep densely-connected network on top
@@ -127,15 +116,48 @@ class AI():
        multiplies with random selection from normal distrubtion with center 
        and SD = mutationRate
     '''
+    def getCurrentEnergy(self,listA,listB):
+        return dict(zip(listA,listB))
+    ''
+    def preprocessEnergyIntake(self,population,energyIntake):
+        currentEnergyIntake = self.getCurrentEnergy(population,energyIntake)
+        if not self.totalEnergyIntake:
+            self.EnergyIntake = self.getCurrentEnergy(population,([0] * len(population)))
+        else:
+            for k, v in currentEnergyIntake.items():
+                self.EnergyIntake[k] = v - self.totalEnergyIntake.get(k, 0)
+            #map energyIntake to 0 or 1
+            for key, value in self.EnergyIntake.items():
+                if value > 0:
+                    self.EnergyIntake[key] = 1
+        self.totalEnergyIntake.update(currentEnergyIntake)
+        return self
 
     def mutateWeights(self, weights, rate):
         mutator = np.random.normal(loc = 0, scale = rate, size = 1)
         return weights + mutator*weights
 
-    '''Input: population is list of all IDs from individuums in population
+    def softmax(self,x):
+        x= np.asarray(x)
+        e_x = np.exp(x - np.max(x))
+        return e_x / e_x.sum()
 
+    def selectSurvivors(self):
+        oldPopulation=list(self.totalEnergyIntake.keys()) 
+        selectionFeature = list(self.totalEnergyIntake.values())
+        try:
+             proba = self.softmax(selectionFeature) 
+        except:
+            survivors = random.choice(oldPopulation,k=len(oldPopulation))
+        else:
+            survivors = list(np.random.choice(oldPopulation, (len(oldPopulation)-int(round(len(oldPopulation)*self.nonSurvivorRate))), p=proba,replace=True))
+        nonSurvivors = list(set(oldPopulation) - set(survivors))
+        for i in range(len(nonSurvivors)):
+            survivors.append(random.choice((nonSurvivors)))
+        return survivors
+    
+    '''Input: population is list of all IDs from individuums in population
        Build models for each member of the population  
-       
        Output: dictionary with key "ID" of individuum and value with "weights" as np.array 
     '''
     def initializeWeights(self, population):
@@ -147,47 +169,55 @@ class AI():
         weightsDict = dict(zip(population,models))
         return weightsDict
 
-    def selectSurvivors(self):
-        s = pd.Series(self.totalEnergyIntake)
-        topN = int(round((len(s)*self.selectionRate),0))
-        print(topN)
-        survivor_df =s.nlargest(topN)
-        survivors =survivor_df.index.tolist()
-        print(self.totalEnergyIntake)
-        self.totalEnergyIntake = {}
-        return survivors
-
     ''' creates weights for individuums of next generation'''
     def reinitializeWeights(self,population,survivors,weightsDict):
-        survivors = self.selectSurvivors()
-        nonSurvivors = list(set(population) - set(survivors))
-        
-        modelWeights = []
-    
-        for id_ in population:
-
-            weights = None
-            if len(modelWeights) > self.survivorRate and len(nonSurvivors) > 0:
-                weights = weightsDict[random.choice(nonSurvivors)]
-            elif len(survivors) == 0:
-                weights = weightsDict[random.choice(nonSurvivors)] 
-            else:
-                weights = weightsDict[random.choice(survivors)]
+        if self.Counter == self.AdaptionFrequency:
+            self.mutationRate = self.mutationRate*self.AdaptionRate
+            self.Counter = 0
+        self.totalEnergyIntake = {}
+        modelWeights = []    
+        for i in range(len(population)):
+            weights = weightsDict[survivors[i]]
             newWeights = self.mutateWeights(weights, self.mutationRate)
             modelWeights.append(np.array(newWeights))
 
         weightsDict = dict(zip(population,modelWeights))
         self.generation = self.generation +1
         return weightsDict
+        
+    '''uses network with inputweights for prediction, returns array'''
+    def prediction(self,weights,inputArray_, auxInput_):
+        self.network.set_weights(weights)
+        pred = self.network.predict([inputArray_, auxInput_])
+        return [float(pred[1][0,0]),float(pred[0][0,0])]
+
+    #create inputs&run pred
+    '''processes Inputs for each Individuum and returns a new output'''
+    def populationOperator(self,population,df,weightsDict):
+        energyIntake = df["energyIntake"].tolist()
+        self.preprocessEnergyIntake(population,energyIntake)
+        outputDict = {}
+        for individuum in population:
+            inputArray, auxInput = self.createInputs(df,individuum)
+            inputArray_, auxInput_ = self.reshaping(inputArray,auxInput)
+            pred_ = self.prediction(weightsDict[individuum],inputArray_, auxInput_)
+            outputDict.update([(individuum,pred_)],)
+        return outputDict 
 
     '''loads model from list with model name'''
     def loadModel(self, population):
+        files_path = os.path.join(self.FilePathModels , '*')
+        files = sorted(glob.iglob(files_path), key=os.path.getctime, reverse=True) 
         models = []
-        for individuum in population:
-            network= load_model('{}{}.h5'.format(self.FilePath, individuum))
-            #model._make_predict_function()
-            models.append(np.array(network.get_weights()))
-        return dict(zip(population,models))
+        if files:
+            for i in range(len(population)):
+                self.network= load_model('{}'.format(random.choice(files)))
+                models.append(np.array(self.network.get_weights()))
+            return dict(zip(population,models))
+
+    def saveModel(self,individuum, weights):
+        self.network.set_weights(weights)
+        self.network.save("{}{}.h5".format(self.FilePathModels,individuum))
 
     def logging(self,fileName):
         log =[self.generation,sum(self.totalEnergyIntake.values() )]
@@ -201,25 +231,3 @@ class AI():
                 writer = csv.writer(f)
                 writer.writerow(log)
         f.close()
-        
-
-
-
-
-    '''uses network with inputweights for prediction, returns array'''
-    def prediction(self,weights,inputArray_, auxInput_):
-        self.network.set_weights(weights)
-        pred = self.network.predict([inputArray_, auxInput_])
-        return [float(pred[1][0,0]),float(pred[0][0,0])]
-
-    #create inputs&run pred
-    '''processes Inputs for each Individuum and returns a new output'''
-    def populationOperator(self,population, df,weightsDict):
-        outputDict = {}
-        self.trackEnergyIntake(df)
-        for individuum in population:
-            inputArray, auxInput = self.createInputs(df,individuum)
-            inputArray_, auxInput_ = self.reshaping(inputArray,auxInput)
-            pred_ = self.prediction(weightsDict[individuum],inputArray_, auxInput_)
-            outputDict.update([(individuum,pred_)],)
-        return outputDict 
