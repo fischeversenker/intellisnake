e219ee9f7e9dcfe5025469cc18af251543936fe5
azacha <41060252+azacha@users.noreply.github.com>
2020-01-22 11:38:59 +0100

    Update websocket-server.py

--- backend/websocket-server.py
+++ backend/websocket-server.py
@@ -1,10 +1,10 @@
 import json
+import pandas as pd
 import websockets
 import asyncio
 import os
 import time
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
-
 from ai import AI
 
 DEBUG_MODE = False
@@ -14,21 +14,19 @@ class WebSocketServer:
     global DEBUG_MODE
 
     def __init__(self):
-        self.ai = AI()
+        self.nes = AI()
         self.started = False
         self.reload = False
-        self.snakesAlive = []
-        self.weightsDict = {}
-        self.model = []
-        self.population = []
-        self.session = ""
-        self.saveEveryXGenerations = 2
-        self.saveCounter = 0
         self.generation = 0
+        self.model = []
+        self.previousMessageData = None
 
     def start(self):
         print("server running...")
         return websockets.serve(self.communication, "localhost", 8765) #change localhost to ip "192.168.1.146"
+        
+    def processMessageData(self, messageData):
+        return pd.DataFrame.from_dict(messageData).T
 
     async def communication(self, websocket, path):
         async for data in websocket:
@@ -39,64 +37,51 @@ class WebSocketServer:
 
         if DEBUG_MODE:
             print("Processing new message:  id: {},  type: {},  len: {}\nState:  started: {}".format(messageId, messageType, len(messageData), str(self.started)))
+        data = self.processMessageData(messageData)
+        print(data)
         if messageType == "generation":
             if not self.started and not self.reload:
                 if DEBUG_MODE:
                     print("starting...")
-
-                self.population = list(messageData["snakeIds"])
-                self.weightsDict = self.ai.initializeWeights(self.population)
-                self.session = str(time.time())
+                self.nes.buildModel()
                 if DEBUG_MODE:
-                    print("done initializeWeights")
+                    print("done...")
                 await self.sendMessage(websocket, messageId, "ack", data = {})
                 self.started = True
 
             if not self.started and self.reload:
+                print("loadModel is not here yet...")
                 if DEBUG_MODE:
                     print("reloading...")
-
-                self.population = list(messageData["snakeIds"])
-                self.weightsDict = self.ai.loadModel(self.population)
-                self.session = str(time.time())
+                #self.model = self.nes.loadModel()
                 if DEBUG_MODE:
-                    print("done reloading Weights")
+                    print("done...")
                 await self.sendMessage(websocket, messageId, "ack", data = {})
                 self.started = True
 
             else:
                 if DEBUG_MODE:
-                    print("new generation...")
-                self.ai.logging(self.session)
-                self.population = list(messageData["snakeIds"])
-                if self.generation == 0:
-                    survivors = self.population
-                else:
-                    survivors = self.ai.selectSurvivors()
-                    self.weightsDict = self.ai.reinitializeWeights(self.population,survivors,self.weightsDict)
-                self.generation = self.generation +1
-                self.saveCounter = self.saveCounter +1
-                if self.saveCounter >= self.saveEveryXGenerations:
-                    for individuum in self.population:
-                        self.ai.saveModel(individuum,self.weightsDict[individuum])
-                    self.saveCounter = 0
+                    print("evole new generation...")
+                
+                if self.generation % 20 == 0:
+                    #self.nes.saveModel(self.model,self.generation)
                     if DEBUG_MODE:
                         print("models saved to file...")
-
-                if DEBUG_MODE:
-                    print("new weights loaded...")
-
-                if isinstance(survivors, list):
-                    await self.sendMessage(websocket, messageId, "ack", survivors)
+                if self.generation == 0:
+                    pass
                 else:
-                    await self.sendMessage(websocket, messageId, "ack", survivors.tolist())
+                    self.nes.logging(self.generation)
+                    self.nes.updateModel()
+                self.generation = self.generation +1
+                if DEBUG_MODE:
+                    print("done...")
+                await self.sendMessage(websocket, messageId, "ack", None)
 
         elif messageType == "data":
             if self.started:
                 if DEBUG_MODE:
                     print("predicting...")
-                df, self.population = self.ai.df_construct(messageData)
-                output_json = self.ai.populationOperator(self.population, df ,self.weightsDict)
+                output_json = self.nes.runModel(data)
                 await self.sendMessage(websocket, messageId, "data", output_json)
             else:
                 await self.sendMessage(websocket, messageId, "error", "you need to send generation message before sending snake data")
