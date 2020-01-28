ef1a47f96f37a55d3454ac57a4ea9076977cc9ef
azacha <41060252+azacha@users.noreply.github.com>
2020-01-28 23:55:23 +0100

    feat: remake evolution mechanism

--- backend/ai.py
+++ backend/ai.py
@@ -1,12 +1,12 @@
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+#os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import numpy as np
 import csv
 import json
 import pandas as pd
 from sklearn.preprocessing import QuantileTransformer
 from PIL import Image
-from keras.layers import Input,Dense, Conv2D, MaxPooling2D, Flatten, BatchNormalization
+from keras.layers import Input, Dense, Conv2D, AveragePooling2D, Flatten, BatchNormalization
 from keras.models import Model
 import time
 # Evolution works as a Black Box Optimization and is based on paper https://arxiv.org/pdf/1703.03864.pdf
@@ -43,26 +43,54 @@ class AI():
         return { "prediction": outputDict, "progress": frameProgress }
 
     def updateModel(self,dict_):
-        A_dict = self.getReward(dict_)
-        A_dict_ = {i:A_dict[i] for i in self.N.keys()} #assure dicts have same order
-        A = list(A_dict_.values())
-        N = list(self.N.values())
+        A = self.getReward(dict_)
         w = self.getModelWeights()
-        self.evoleNetwork(A,N,w)
+        self.evoleModel(A,w)
         self.population = None
         self.W_try = None
         self.N = None
         self.frameCount = 0
 
+    def weighting(self,A,N):
+        for element in list(A.keys()):
+            w_weighted = N[element]
+            loss = A[element]
+           
+            for i in range(len(w_weighted)):
+                w_weighted[i][0] = w_weighted[i][0]*loss
+                w_weighted[i][1] = w_weighted[i][1]*loss
+   
+            N[element] = w_weighted
+        return N
+
+    def weightedSumWeights(self,N,w):
+        w_add = w
+        for i in range(len(w_add)):
+            w_add[i][0] = np.zeros(w_add[i][0].shape)
+            w_add[i][1] = np.zeros(w_add[i][0].shape)
+            for element in list(N.keys()):
+                w_add[i][0] = N[element][i][0] + w_add[i][0]
+                w_add[i][0] = N[element][i][0] + w_add[i][0]
+            w_add[i][0] = (self.alpha/(len(self.population)*self.sigma))*w_add[i][0]
+            w_add[i][1] = (self.alpha/(len(self.population)*self.sigma))*w_add[i][1]
+        return w_add
+
+    def evoleModel(self,A,w):
+        N = self.weighting(A,self.N)
+        w_add = self.weightedSumWeights(N,w)
+        w = w + w_add
+        self.model.set_weights(w)
+
     def buildModel(self):
         input_ = Input(shape=(self.shape,self.shape,3))
-        encoder = Conv2D(16, kernel_size=(8, 8),strides = (4,4), padding='same',activation ='relu')(input_)
-        encoder = MaxPooling2D((2, 2))(encoder)
-        encoder = Conv2D(32, kernel_size=(4, 4),strides =(2,2), padding='same',activation ='relu')(encoder)
-        encoder = MaxPooling2D((2, 2))(encoder)
+
+        encoder = Conv2D(16, kernel_size=(8, 8), strides = (4,4), padding='same', activation ='relu', kernel_initializer='he_uniform', bias_initializer='zeros' )(input_)
+        encoder = AveragePooling2D((2, 2))(encoder)
+        encoder = Conv2D(32, kernel_size=(4, 4),strides =(2,2), padding='same',activation ='relu', kernel_initializer='he_uniform', bias_initializer='zeros')(encoder)
+        encoder = AveragePooling2D((2, 2))(encoder)
         x = Flatten()(encoder)
-        x = Dense(units= 32, activation = 'relu')(x)
-        output = Dense(units = 2, activation='tanh')(x) 
+        x = Dense(units= 32, activation = 'tanh', kernel_initializer='glorot_uniform', bias_initializer='zeros')(x)
+        output = Dense(units = 2, activation='tanh', kernel_initializer='glorot_uniform', bias_initializer='zeros')(x) 
         self.model = Model(inputs=[input_], outputs=[output])
         self.model.compile(optimizer='adam', loss='binary_crossentropy',loss_weights=[0.1])
 
@@ -94,26 +122,21 @@ class AI():
             A = (R - np.mean(R)) / np.std(R) # map to gaussian distribution
         return dict(zip(keys,A))
 
-    def evoleNetwork(self,A,N,w):
-        for i in range(len(w)):
-            n_ = np.dot( np.array(N)[:,i].T,A) #sum up all the rows of noise matrix for each layer and each row is weighted by A
-            n_ = np.reshape(n_,w[i].shape)
-            w[i] = w[i] + (self.alpha/(len(self.population)*self.sigma))*n_
-        self.model.set_weights(w)
-
     def createNoiseMatrix(self):
         w = self.getModelWeights()
         self.N = {}
         for element in self.population:
-            n = [None] * len(w)
+            n = w
             for j in range(len(w)):
-                n[j] = np.random.rand(w[j].size) #adjust weights layer-wise
+                n[j][0] = np.array(np.reshape(np.random.rand(w[j][0].size),w[j][0].shape),dtype= np.float64) #adjust weights layer-wise
+                n[j][1] = np.array(np.reshape(np.random.rand(w[j][1].size),w[j][0].shape),dtype= np.float64) #adjust biases layer-wise
             self.N.update([(element,n)],)
 
     def mutateWeights(self, n):
         w = self.getModelWeights()
         for i in range(len(n)):
-            w[i]= w[i] + self.sigma*np.reshape(n[i],w[i].shape) #mutate weights of layers
+            w[i][0] = w[i][0] + self.sigma*np.reshape(n[i][0],w[i][0].shape) #mutate weights of layers
+            w[i][1] = w[i][1] + self.sigma*np.reshape(n[i][1],w[i][1].shape) #mutate biases of layers
         return w
 
     def applyNoise(self):
